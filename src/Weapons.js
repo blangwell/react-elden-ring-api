f0cd6e76caf5e03031e3ac6229d931d18313b03a
blangwell <b.langwell@outlook.com>
2022-05-29 13:31:48 -0700

    GET request to api on initial render, add axios dependency

--- src/Weapons.js
+++ src/Weapons.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export default function Weapons() {
+	const [weapons, setWeapons] = useState(null);
+
+	/* On initial render:
+	Make GET request to Elden Ring fan API
+	set weapons state with response data array */
+	useEffect(() => {
+		axios.get('https://eldenring.fanapis.com/api/weapons?limit=10')
+		.then(response => {
+			setWeapons(response.data.data)
+		})
+		.catch(error => {
+			console.error(error);
+			setWeapons(null);
+		});
+	});
+	
 	return (
 		<div>
 			<h1>Elden Ring Weapons</h1>
+			{ weapons ? weapons.map(weapon => <p key={weapon.id}>{weapon.name}</p>): null}
 		</div>
-	)
+	);
 }
