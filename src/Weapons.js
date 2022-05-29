e5a2ef095ba3d419a5f7bb766d1308868db12761
blangwell <b.langwell@outlook.com>
2022-05-29 16:06:26 -0700

    Install Bootstrap, update old css, remove index.css, update page title

--- src/Weapons.js
+++ src/Weapons.js
@@ -1,6 +1,13 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import {
+	Container,
+	Row,
+	Col,
+} from 'react-bootstrap';
+
 import ResultCard from './ResultCard';
+import './Weapons.css';
 
 export default function Weapons() {
 	const [weapons, setWeapons] = useState([]);
@@ -37,56 +44,62 @@ export default function Weapons() {
 	}, [filter]);
 	
 	return (
-		<div>
+		<Container>
 			<h1>Elden Ring Weapons</h1>
-			<form onChange={e => setFilter(e.target.value)}>
-			 	{/* use fieldset for accessibility https://www.w3.org/WAI/tutorials/forms/grouping/ */}
-				<fieldset>
-					<legend>Weapon Category</legend>
-					<label htmlFor="category">
-						<input defaultChecked type="radio" value="" name="category" id="all" />
-						All	
-					</label>
-					<label htmlFor="category">
-						<input type="radio" value="axe" name="category" id="axe" />
-						Axe
-					</label>
-					<label htmlFor="category">
-						<input type="radio" value="bow" name="category" id="bow" />
-						Bow
-					</label>
-					<label htmlFor="category">
-						<input type="radio" value="colossal sword" name="category" id="colossal-sword" />
-						Colossal Sword
-					</label>
-					<label htmlFor="category">
-						<input type="radio" value="colossal weapon" name="category" id="colossal-weapon" />
-						Colossal Weapon
-					</label>
-					<label htmlFor="category">
-						<input type="radio" value="crossbow" name="category" id="crossbow" />
-						Crossbow
-					</label>
-					<label htmlFor="category">
-						<input type="radio" value="curved greatsword" name="category" id="curved-greatsword" />
-						Curved Greatsword
-					</label>
-					<label htmlFor="category">
-						<input type="radio" value="curved sword" name="category" id="curved-sword" />
-						Curved Sword
-					</label>
-					<label htmlFor="category">
-						<input type="radio" value="dagger" name="category" id="dagger" />
-						Dagger
-					</label>
-					<label htmlFor="category">
-						<input type="radio" value="fist" name="category" id="fist" />
-						Fist
-					</label>
-				</fieldset>
-			</form>
-			{/* display weapons, or load message */}
-			{ displayWeapons ? displayWeapons.map(weapon => <ResultCard key={weapon.name} weapon={weapon} />): <h2>Fetching Weapons...</h2> }
-		</div>
+			<Row>
+				<Col md={4} id="filters">
+					<form onChange={e => setFilter(e.target.value)}>
+						{/* use fieldset for accessibility https://www.w3.org/WAI/tutorials/forms/grouping/ */}
+						<fieldset>
+							<legend>Weapon Category</legend>
+							<label htmlFor="category">
+								<input defaultChecked type="radio" value="" name="category" id="all" />
+								All	
+							</label>
+							<label htmlFor="category">
+								<input type="radio" value="axe" name="category" id="axe" />
+								Axe
+							</label>
+							<label htmlFor="category">
+								<input type="radio" value="bow" name="category" id="bow" />
+								Bow
+							</label>
+							<label htmlFor="category">
+								<input type="radio" value="colossal sword" name="category" id="colossal-sword" />
+								Colossal Sword
+							</label>
+							<label htmlFor="category">
+								<input type="radio" value="colossal weapon" name="category" id="colossal-weapon" />
+								Colossal Weapon
+							</label>
+							<label htmlFor="category">
+								<input type="radio" value="crossbow" name="category" id="crossbow" />
+								Crossbow
+							</label>
+							<label htmlFor="category">
+								<input type="radio" value="curved greatsword" name="category" id="curved-greatsword" />
+								Curved Greatsword
+							</label>
+							<label htmlFor="category">
+								<input type="radio" value="curved sword" name="category" id="curved-sword" />
+								Curved Sword
+							</label>
+							<label htmlFor="category">
+								<input type="radio" value="dagger" name="category" id="dagger" />
+								Dagger
+							</label>
+							<label htmlFor="category">
+								<input type="radio" value="fist" name="category" id="fist" />
+								Fist
+							</label>
+						</fieldset>
+					</form>
+				</Col>
+				<Col md={8} id="results">
+					{/* display weapons, or load message */}
+					{ displayWeapons ? displayWeapons.map(weapon => <ResultCard key={weapon.name} weapon={weapon} />): <h2>Fetching Weapons...</h2> }
+				</Col>
+			</Row>
+		</Container>
 	);
 }
