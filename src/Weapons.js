1c6b09a01d4df5136c0600ebbdc6e9997d007957
blangwell <b.langwell@outlook.com>
2022-05-30 16:38:21 -0700

    Add accordion for category links

--- src/Weapons.js
+++ src/Weapons.js
@@ -1,69 +1,43 @@
-import { useState, useEffect } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import {
 	Container,
 	Row,
 	Col,
+	Accordion,
+	ListGroup,
 } from 'react-bootstrap';
 import categories from './categories';
 import './Weapons.css';
 
 export default function Weapons() {
-	/* On initial render:
-	Make GET request to Elden Ring fan API
-	set weapons state with response da:w
-	ta array */
-	// useEffect(() => {
-	// 	const query = gql`
-	// 	{
-	// 		weapon(category: "Axe") {
-	// 			id,
-	// 			name,
-	// 			description,
-	// 			image, 
-	// 			scalesWith {
-	// 				name,
-	// 				scaling
-	// 			},
-	// 		}
-	// 	}
-	// 	`
-	// 	request('https://eldenring.fanapis.com/api/graphql', query)
-	// 	.then(data => {
-	// 		console.log(data.weapon)
-	// 		setWeapons(data.weapon);
-	// 		setDisplayWeapons(data.weapon);
-	// 	})
-	// 	.catch(error => {
-	// 		console.error(error);
-	// 		setWeapons(null);
-	// 	});
-	// }, []);
-
-	// // each time the filter is updated, filter weapons and setDisplayWeapons
-	// useEffect(() => {
-	// 	if (!filter) {
-	// 		setDisplayWeapons(weapons);
-	// 	} else {
-	// 		let filtered = weapons.filter(weapon => {
-	// 			return weapon.category.toLowerCase() === filter || 
-	// 			// or check because some categories are pluralized 
-	// 			weapon.category.toLowerCase() === filter + 's';
-	// 		});
-	// 		setDisplayWeapons(filtered);
-	// 	}
-	// }, [filter]);
-	
 	return (
 		<Container>
-			<nav>
-				{categories.map(category => (
-					<Link to={`/weapons/${category}`} key={category}>
-					{category}
-					</Link>
-				))}
-			</nav>
-			<Outlet />
+			<Row>
+				<Col md="3">
+
+					<Accordion flush>
+						<Accordion.Item eventKey="0">
+							<Accordion.Header>Categories</Accordion.Header>
+							<Accordion.Body>
+							<nav>
+							<ListGroup>
+								{categories.map(category => (
+									<ListGroup.Item action href={category}>
+										<Link to={`/weapons/${category}`} key={category}>
+										{category}
+										</Link>
+									</ListGroup.Item>
+								))}	
+							</ListGroup>
+							</nav>
+							</Accordion.Body>
+						</Accordion.Item>
+					</Accordion>
+				</Col>
+				<Col md="9">
+					<Outlet />
+				</Col>
+			</Row>
 		</Container>
 	);
 }
