17047a11a99b41fa02ef9d91c334a63955aa4a1b
blangwell <b.langwell@outlook.com>
2022-05-30 15:45:21 -0700

    Clean up WeaponCategory

--- src/WeaponCategory.js
+++ src/WeaponCategory.js
@@ -8,27 +8,24 @@ export default function Weapon(props) {
 	let params = useParams();
 
 	// query graphql service for all weapons in params category
-	const query = gql`
-		query getWeapon($weaponCategory: String!) {
-			weapon(category: $weaponCategory) {
-				id,
-				name,
-				description,
-				image, 
-				scalesWith {
-					name,
-					scaling
-				}
-			}
-		}
-	`
-		
+	
 	// store weapons in state on initial render
 	useEffect(() => {
-		const variables = {
-			weaponCategory: params.weaponCategory
-		}
-		console.log(params.weaponCategory);
+		const query = gql`
+			query getWeapon($weaponCategory: String!) {
+				weapon(category: $weaponCategory) {
+					id,
+					name,
+					description,
+					image, 
+					scalesWith {
+						name,
+						scaling
+					}
+				}
+			}
+		`
+		const variables = { weaponCategory: params.weaponCategory }
 		request('https://eldenring.fanapis.com/api/graphql', query, variables)
 		.then(data => {
 			setWeapons(data.weapon);
@@ -45,7 +42,6 @@ export default function Weapon(props) {
 			{weapons ? weapons.map(weapon => (
 				<ResultCard key={weapon.name} weapon={weapon} />
 			)) : <h1>Fetching Weapons...</h1>}
-
 		</div>
 	)
 }
